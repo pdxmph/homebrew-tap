97812dcd7741d27719ab761741bc9a5edf302594
Mike Hall <mike@puddingtime.org>
2025-07-11 08:51:06 -0700

    denote-tasks v0.14.1

---
+++ Formula/denote-tasks.rb
@@ -0,0 +1,52 @@
+class DenoteTasks < Formula
+  desc "Task management tool using Denote file naming convention"
+  homepage "https://github.com/pdxmph/denote-tasks"
+  version "0.14.1"
+  license "MIT"
+  
+  # Build from source by default
+  url "https://github.com/pdxmph/denote-tasks/archive/refs/tags/v0.14.1.tar.gz"
+  sha256 "26715011d330b8bd129c75bb5c280efe836d2f8d74ea027ba6d0a3bfea984ee4"
+  
+  # Binary releases for faster installation
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/pdxmph/denote-tasks/releases/download/v0.14.1/denote-tasks_v0.14.1_darwin_arm64.tar.gz"
+    sha256 "1dc50281130fcf9b37d84abff0a361940e1cb5abe85211a50aeee33a3c072a18"
+  end
+  
+  depends_on "go" => :build if build.from_source?
+  depends_on arch: :arm64  # Currently only ARM64 builds available
+
+  def install
+    if build.from_source?
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+      
+      # Install completions from source
+      bash_completion.install "completions/denote-tasks.bash"
+      zsh_completion.install "completions/_denote-tasks"
+    else
+      # Install pre-built binary
+      bin.install "denote-tasks"
+      
+      # Install completions from binary archive
+      bash_completion.install "completions/denote-tasks.bash" if File.exist?("completions/denote-tasks.bash")
+      zsh_completion.install "completions/_denote-tasks" if File.exist?("completions/_denote-tasks")
+    end
+    
+    # Install documentation
+    doc.install "README.md" if File.exist?("README.md")
+  end
+
+  def caveats
+    <<~EOS
+      To use denote-tasks, create ~/.config/denote-tasks/config.toml with:
+
+        notes_directory = "~/tasks"
+        editor = "vim"  # or your preferred editor
+    EOS
+  end
+
+  test do
+    assert_match "denote-tasks", shell_output("#{bin}/denote-tasks --help 2>&1")
+  end
+end
